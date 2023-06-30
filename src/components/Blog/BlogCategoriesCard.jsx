e14ec372370f4f7b86e70091ad9c140132177289
Syed Huzaifa Abid <62389218+huzaifa26@users.noreply.github.com>
2023-06-30 15:45:00 +0500

    fix

--- src/components/Blog/BlogCategoriesCard.jsx
+++ src/components/Blog/BlogCategoriesCard.jsx
@@ -1,12 +1,13 @@
+import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 
 export default function BlogCategoriesCard({ img, title,type="svg" }) {
   return (
-    <Link href={title}>
+    <Link href={'/blog/'+title}>
       <div style={{ background: 'linear-gradient(95.49deg, #FFDBB8 2.04%, rgba(255, 187, 121, 0.8) 99.48%)' }} className='h-[134px] flex gap-[10px] rounded-[15px] justify-center items-center'>
       {/* <div style={{ background: 'linear-gradient(95.49deg, #FFDBB8 2.04%, rgba(255, 187, 121, 0.8) 99.48%)' }} className='w-[424px] h-[134px] flex gap-[10px] rounded-[15px] justify-center items-center'> */}
-        {type === "svg"? img :<img className='w-[32px]' src={img} alt="" />}
+        {type === "svg"? img :<Image className='w-[32px]' src={img} alt="" />}
         <p className='font-rubik font-medium text-[28px] md:text-[24px] md:leading-[31.44px] sm:text-[24px] sm:leading-[31.44px] leading-[36.68px] tracking-[-0.02em]'>{title}</p>
       </div>
     </Link>
