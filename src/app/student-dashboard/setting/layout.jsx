44533196fa20f80b39c96ad080328d28193d20bc
Syed Huzaifa Abid <62389218+huzaifa26@users.noreply.github.com>
2023-07-09 22:38:01 +0500

    fix

--- src/app/student-dashboard/setting/layout.jsx
+++ src/app/student-dashboard/setting/layout.jsx
@@ -1,20 +1,28 @@
 'use client'
+import Link from 'next/link'
 import { usePathname } from 'next/navigation'
 import React from 'react'
 
-export default function layout() {
-  const pathname=usePathname()
+export default function layout({ children }) {
+  const pathname = usePathname()
   return (
     <div className='px-[12.153vw] flex gap-x-[20px] mt-[50px]'>
       <div className='flex flex-col gap-[25px] mt-[42px]'>
-        <button className={pathname === '/student-dashboard/setting' ? 'text-[white] font-outfit text-[20px] font-semibold h-[55px] text-left px-[16px] w-[281px] rounded-[8px] bg-primary2' :'text-[#252422] font-outfit text-[20px] font-semibold h-[55px] text-left px-[16px] border-b border-[#FFDBB8] w-[281px]'}>Account</button>
-        <button className='text-[#252422] font-outfit text-[20px] font-semibold h-[55px] text-left px-[16px] border-b border-[#FFDBB8] w-[281px]'>Email & Password</button>
-        <button className='text-[#252422] font-outfit text-[20px] font-semibold h-[55px] text-left px-[16px] border-b border-[#FFDBB8] w-[281px]'>Payment methods</button>
-        <button className='text-[#252422] font-outfit text-[20px] font-semibold h-[55px] text-left px-[16px] border-b border-[#FFDBB8] w-[281px]'>Notification Settings</button>
+        <Link href="/student-dashboard/setting">
+          <button className={pathname === '/student-dashboard/setting' ? 'text-[white] font-outfit text-[20px] font-semibold h-[55px] text-left px-[16px] w-[281px] rounded-[8px] bg-primary2' : 'text-[#252422] font-outfit text-[20px] font-semibold h-[55px] text-left px-[16px] border-b border-[#FFDBB8] w-[281px]'}>Account</button>
+        </Link>
+        <Link href="/student-dashboard/setting/user-info">
+          <button className={pathname === '/student-dashboard/setting/user-info' ? 'text-[white] font-outfit text-[20px] font-semibold h-[55px] text-left px-[16px] w-[281px] rounded-[8px] bg-primary2' : 'text-[#252422] font-outfit text-[20px] font-semibold h-[55px] text-left px-[16px] border-b border-[#FFDBB8] w-[281px]'}>Email & Password</button>
+        </Link>
+        <Link href="/student-dashboard/setting/payment">
+          <button className={pathname === '/student-dashboard/setting/payment' ? 'text-[white] font-outfit text-[20px] font-semibold h-[55px] text-left px-[16px] w-[281px] rounded-[8px] bg-primary2' : 'text-[#252422] font-outfit text-[20px] font-semibold h-[55px] text-left px-[16px] border-b border-[#FFDBB8] w-[281px]'}>Payment methods</button>
+        </Link>
+        <Link href="/student-dashboard/setting/notification">
+          <button className={pathname === '/student-dashboard/setting/notification' ? 'text-[white] font-outfit text-[20px] font-semibold h-[55px] text-left px-[16px] w-[281px] rounded-[8px] bg-primary2' : 'text-[#252422] font-outfit text-[20px] font-semibold h-[55px] text-left px-[16px] border-b border-[#FFDBB8] w-[281px]'}>Notification Settings</button>
+        </Link>
       </div>
-      <div>
 
-      </div>
+      {children}
     </div>
   )
 }
--- src/app/student-dashboard/setting/user-info/page.jsx
+++ src/app/student-dashboard/setting/user-info/page.jsx
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function page() {
   return (
-    <div>account</div>
+    <div>user-info</div>
   )
 }
